8d2bbad0d004c06a3e3a63cc9c85fac77387a08e
juanchi <ansaldijuanc@hotmail.com>
2020-08-06 20:14:36 -0300

    send record token

--- packages/pi-station/src/sendRecord.js
+++ packages/pi-station/src/sendRecord.js
@@ -54,6 +54,7 @@ function postData(data) {
         accept: "application/json",
         "Accept-Language": "en-US,en;q=0.8",
         "Content-Type": "application/json",
+        "x-access-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMjVkZWVjNDU1Yzg0MzM3MGFjMDNmNiIsImlhdCI6MTU5NjMxNzQyMH0.wHgJX2chNq7ZKycv4CzsAKOyBg-aZn7dvMZ7bpUjowc"
       },
       maxContentLength: Infinity,
       maxBodyLength: Infinity,
