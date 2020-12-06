d14cc08de7f6d2bd46f7785b3ee0750a13a2299c
juanchi <ansaldijuanc@hotmail.com>
2020-12-06 17:45:14 -0300

    Prod

--- packages/server/api/routes/index.js
+++ packages/server/api/routes/index.js
@@ -6,9 +6,4 @@ const { allowNonAuthenticated } = require("../config/auth");
 // Welcome Page
 router.get('/', allowNonAuthenticated, (req, res) => { res.sendFile(path.resolve('views/index.html'));}) //res.render('login.ejs', { message: req.flash('loginMessage') }));
 
-
-// router.get("/", (req, res) => {
-//   res.sendFile(path.resolve('views/index.html'));
-// });
-
 module.exports = router;
