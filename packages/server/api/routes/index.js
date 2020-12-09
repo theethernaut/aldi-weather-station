4cea7a4296b98a30d8eaa31279c38717d612d365
juanchi <ansaldijuanc@hotmail.com>
2020-12-09 00:26:38 -0300

    Fixes

--- packages/server/api/routes/index.js
+++ packages/server/api/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const path = require('path');
-const { allowNonAuthenticated } = require("../config/auth");
+const { allowNonAuthenticated, ensureAuthenticated } = require("../config/auth");
 
 // Welcome Page
 router.get('/', allowNonAuthenticated, (req, res) => { res.sendFile(path.resolve('views/index.html'));}) //res.render('login.ejs', { message: req.flash('loginMessage') }));
+
+router.get('/home', ensureAuthenticated, (req, res) => { res.sendFile(path.resolve('views/index.html'));}) //res.render('login.ejs', { message: req.flash('loginMessage') }));
 
 module.exports = router;
